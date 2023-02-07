8aa8a4dc2d7023b85032f044033cb8951697431a
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2023-02-07 18:54:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def start(update, context):
 def main():
     """Start the bot."""
     dp.add_handler(CommandHandler("start", start))
+    updater.start_polling(timeout=15, read_latency=4, drop_pending_updates=True)
+    updater.idle()
 
 if __name__ == '__main__':
     main()
